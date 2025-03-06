8ed52d93cbaa7203967ec2091a2194000fcc7885
Calinightshift <calinightshift@gmail.com>
2025-03-06 09:21:18 -0800

    Initial project setup

---
+++ terraform/variables.tf
@@ -0,0 +1,26 @@
+variable "subscription_id" {
+  type    = string
+  default = "07f2e3d2-4f84-4699-8505-3478b00ea035"
+}
+
+variable "client_id" {
+  type    = string
+  default = "e887143d-429e-4bd5-8f28-f28bf35b4650"
+}
+
+variable "client_secret" {
+  type    = string
+  default = "NFQ8Q~Vbeac38iYK6fO.kXAUD9w09OaKjrmjBc60"
+}
+
+variable "tenant_id" {
+  type    = string
+  default = "674d3e6c-45f7-42e9-938c-fcd01e3ff57c"
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for VM access"
+  type        = string
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCXwxHon0chWpcVV3S8HJKRGgJhyXgFw49AQsyoU87PKw6hu11HPRAU+5fYwTwt39RBgQyKsrbwj+681LcG0/yVdxBG56OKaPrkX0ALcaUukd0Jqd1Xdbf5f1IEfBqD3+JExcRQZMXJv6CqLetvElVg8+Hw0NNcEzgw2FV3yswvGrXCRzWPfY8A0FkJzK7KvU0xYJ73+cx0KqyxomiP6xTitGcewhG+sKkmGu3KBmXhUGyMQSjUjGQDMUAXF/riMjJ84OGEkUXbPPlv/e7eh5fw6te3stAwRCJk1weKDAwL0D6SN0qx1Nc4kjBPbFx8enaTMJC8QftnIQ1SsMS92Yk1DrE16LGohB6a5HuIUrkCJ0fzU6XUyg6IiJ6+X8EHUaBs8HGAKGq9OffQvenn8zuxlF7BNkrZrHR1shw3N07B5NFDdK7+su77yBgO2o5jO8bPdxAVppDxiqMCVc6EGusrOv1LYdQCJb9ayouPci+qbl+BdyzePK3LjOI5yne/tdJ6vH3T8wM11AlQI0eb8jLHLPoQosw7gSSPYNeUoVZMCYfSNEehYEjRFAgDbRu50Wmw0CCwcJg9pdxSUg3p0BQBCfRZ/hdRztkGX16IXrOifrcLV+506m16vfHa3W+xTbWnBYSR29eObnz8UpIg+VusDfn6wvjmI5Mz497xAHpB0w== neilr@BigNDeezl"
+}
+
